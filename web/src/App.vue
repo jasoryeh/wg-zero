ed225ec28a33974f2fe34f3ad1b528ee8bfcd7df
Jason Ho <git@hogt.me>
2023-03-31 06:58:10 +0000

    server reloads and saves

--- web/src/App.vue
+++ web/src/App.vue
@@ -38,6 +38,20 @@ import Login from './components/Login.vue'
             <p class="text-2xl font-medium">Clients</p>
           </div>
           <div class="flex-shrink-0">
+            <button @click="this.reloadServer()"
+              class="hover:bg-red-800 hover:border-red-800 hover:text-white text-gray-700 border-2 border-gray-100 py-2 px-4 rounded inline-flex items-center transition">
+              <svg class="w-4 mr-2" inline xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M17.65 6.35a7.95 7.95 0 0 0-6.48-2.31c-3.67.37-6.69 3.35-7.1 7.02C3.52 15.91 7.27 20 12 20a7.98 7.98 0 0 0 7.21-4.56c.32-.67-.16-1.44-.9-1.44c-.37 0-.72.2-.88.53a5.994 5.994 0 0 1-6.8 3.31c-2.22-.49-4.01-2.3-4.48-4.52A6.002 6.002 0 0 1 12 6c1.66 0 3.14.69 4.22 1.78l-1.51 1.51c-.63.63-.19 1.71.7 1.71H19c.55 0 1-.45 1-1V6.41c0-.89-1.08-1.34-1.71-.71l-.64.65z"/>
+              </svg>
+              <span class="text-sm">Reload</span>
+            </button>
+            <button @click="this.commitServer()"
+              class="hover:bg-red-800 hover:border-red-800 hover:text-white text-gray-700 border-2 border-gray-100 py-2 px-4 rounded inline-flex items-center transition">
+              <svg class="w-4 mr-2" inline xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M21 7v12q0 .825-.588 1.413T19 21H5q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h12l4 4Zm-9 11q1.25 0 2.125-.875T15 15q0-1.25-.875-2.125T12 12q-1.25 0-2.125.875T9 15q0 1.25.875 2.125T12 18Zm-6-8h9V6H6v4Z"/>
+              </svg>
+              <span class="text-sm">Save</span>
+            </button>
             <button @click="clientCreate = true; clientCreateName = '';"
               class="hover:bg-red-800 hover:border-red-800 hover:text-white text-gray-700 border-2 border-gray-100 py-2 px-4 rounded inline-flex items-center transition">
               <svg class="w-4 mr-2" inline xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -504,7 +518,13 @@ export default {
     },
     async updateClientName(client, name) {
       await this.api.updateName(client.Reference, name);
-    }
+    },
+    async reloadServer() {
+      await this.api.reload();
+    },
+    async commitServer() {
+      await this.api.save();
+    },
   },
   filters: {
     bytes,
