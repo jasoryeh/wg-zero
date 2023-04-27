c2f5a4daed59102a6bc45cad9e3d16e37799d094
Jason Ho <git@hogt.me>
2023-04-27 15:18:37 -0700

    adjust web ui's credits to finally include me

--- web/src/components/Credits.vue
+++ web/src/components/Credits.vue
@@ -2,10 +2,29 @@
 </script>
 
 <template>
-  <p v-cloak class="text-center m-10 text-gray-300 text-xs">Made by <a target="_blank" class="hover:underline"
-        href="https://emilenijssen.nl/?ref=wg-easy">Emile Nijssen</a> · <a class="hover:underline"
-        href="https://github.com/sponsors/WeeJeWel" target="_blank">Donate</a> · <a class="hover:underline"
-        href="https://github.com/weejewel/wg-easy" target="_blank">GitHub</a></p>
+  <div class="m-10">
+    <p v-cloak class="text-center text-gray-300 text-xs">
+      wg-easy
+      &nbsp;·&nbsp;
+      <a class="hover:underline" href="https://github.com/jasoryeh/wg-easy" target="_blank">Fork Source Code</a>
+      &nbsp;·&nbsp;
+      <a class="hover:underline" href="https://github.com/WeeJeWel/wg-easy" target="_blank">Original Source Code</a>
+    </p>
+    <p v-cloak class="text-center text-gray-300 text-xs">
+      Forked by
+      <a target="_blank" class="hover:underline" href="https://hogt.me/?ref=wg-easy">Jason Ho</a>
+      &nbsp;·&nbsp;
+      <a class="hover:underline" href="https://github.com/jasoryeh" target="_blank">Profile</a>
+    </p>
+    <p v-cloak class="text-center text-gray-300 text-xs">
+      Originally by
+      <a target="_blank" class="hover:underline" href="https://emilenijssen.nl/?ref=wg-easy">Emile Nijssen</a>
+      &nbsp;·&nbsp;
+      <a class="hover:underline" href="https://github.com/sponsors/WeeJeWel" target="_blank">Donate</a>
+      &nbsp;·&nbsp;
+      <a class="hover:underline" href="https://github.com/WeeJeWel" target="_blank">Profile</a>
+    </p>
+  </div>
 </template>
 
 <style scoped>
