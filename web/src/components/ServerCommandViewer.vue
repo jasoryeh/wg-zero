06ea83a7a3ce6ebc2da6f04a3495ccd2bb92aa15
Jason Ho <git@hogt.me>
2025-01-11 23:23:48 -0800

    round code viewer in server command viewer, make it show actual commands

--- web/src/components/ServerCommandViewer.vue
+++ web/src/components/ServerCommandViewer.vue
@@ -11,23 +11,23 @@ import Modal from './Modal.vue';
             <div>
                 <div class="bg-neutral-100 dark:bg-neutral-700 p-4 mt-2 rounded-md">
                     <h5 class="text-md font-bold mb-2">Before Starting</h5>
-                    <pre class="p-4 bg-white dark:bg-black  dark:text-white" v-if="PreUp && PreUp.length > 0">{{ PreUp }}</pre>
-                    <pre class="p-4 bg-white dark:bg-black dark:text-white" v-else>No commands are run in this phase.</pre>
+                    <pre class="p-4 bg-white dark:bg-black dark:text-white rounded-md" v-if="PreUp && PreUp.length > 0">{{ PreUp.join('\n') }}</pre>
+                    <pre class="p-4 bg-white dark:bg-black dark:text-white rounded-md" v-else>No commands are run in this phase.</pre>
                 </div>
                 <div class="bg-neutral-100 dark:bg-neutral-700 p-4 mt-2 rounded-md">
                     <h5 class="text-md font-bold mb-2">After Starting</h5>
-                    <pre class="p-4 bg-white dark:bg-black dark:text-white" v-if="PostUp && PostUp.length > 0">{{ PostUp }}</pre>
-                    <pre class="p-4 bg-white dark:bg-black dark:text-white" v-else>No commands are run in this phase.</pre>
+                    <pre class="p-4 bg-white dark:bg-black dark:text-white rounded-md" v-if="PostUp && PostUp.length > 0">{{ PostUp.join('\n') }}</pre>
+                    <pre class="p-4 bg-white dark:bg-black dark:text-white rounded-md" v-else>No commands are run in this phase.</pre>
                 </div>
                 <div class="bg-neutral-100 dark:bg-neutral-700 p-4 mt-2 rounded-md">
                     <h5 class="text-md font-bold mb-2">Before Stopping</h5>
-                    <pre class="p-4 bg-white dark:bg-black dark:text-white" v-if="PreDown && PreDown.length > 0">{{ PreDown }}</pre>
-                    <pre class="p-4 bg-white dark:bg-black dark:text-white" v-else>No commands are run in this phase.</pre>
+                    <pre class="p-4 bg-white dark:bg-black dark:text-white rounded-md" v-if="PreDown && PreDown.length > 0">{{ PreDown.join('\n') }}</pre>
+                    <pre class="p-4 bg-white dark:bg-black dark:text-white rounded-md" v-else>No commands are run in this phase.</pre>
                 </div>
                 <div class="bg-neutral-100 dark:bg-neutral-700 p-4 mt-2 rounded-md">
                     <h5 class="text-md font-bold mb-2">After Stopping</h5>
-                    <pre class="p-4 bg-white dark:bg-black dark:text-white" v-if="PostDown && PostDown.length > 0">{{ PostDown }}</pre>
-                    <pre class="p-4 bg-white dark:bg-black dark:text-white" v-else>No commands are run in this phase.</pre>
+                    <pre class="p-4 bg-white dark:bg-black dark:text-white rounded-md" v-if="PostDown && PostDown.length > 0">{{ PostDown.join('\n') }}</pre>
+                    <pre class="p-4 bg-white dark:bg-black dark:text-white rounded-md" v-else>No commands are run in this phase.</pre>
                 </div>
             </div>
         </div>
